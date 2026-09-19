52e44c049680b5066a9f44efd21c34b28e8d8f6e
dzambrano023 <dzambranogr@gmail.com>
2026-09-19 14:53:36 -0500

    feat: Creacion de estructura HTML en archivo logica.js se crean las constantes y ciclo for

---
+++ src/login.js
@@ -0,0 +1,9 @@
+const USUARIO = admin;
+const CONTRASENA = 12345;
+
+for (let i = 0; i < 3; i++) {
+let usuarioIngresado = prompt("Ingrese su usuario:");
+let contrasenaIngresada = prompt("Ingrese su contraseña:");
+
+
+}
