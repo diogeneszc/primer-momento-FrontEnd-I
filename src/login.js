93a92181e23edacc1ea12221420c3fa185c14625
dzambrano023 <dzambranogr@gmail.com>
2026-09-19 20:10:51 -0500

    feat: implementacion de la funcion validarAcesso en el modulo principal

--- src/login.js
+++ src/login.js
@@ -1,9 +1,2 @@
-const USUARIO = admin;
-const CONTRASENA = 12345;
-
-for (let i = 0; i < 3; i++) {
-let usuarioIngresado = prompt("Ingrese su usuario:");
-let contrasenaIngresada = prompt("Ingrese su contraseña:");
-
-
-}
+import validarAcceso from "./funciones.js";
+validarAcceso ();
